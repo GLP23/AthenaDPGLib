dd38070796526d5c0ab783f55fa434f4078d723f
DirectiveAthena <directive.athena@gmail.com>
2022-06-08 02:02:14 +0200

    Refactor Rename of activity to Application

--- src/AthenaGuiLib/entities/__init__.py
+++ src/AthenaGuiLib/entities/__init__.py
@@ -2,7 +2,9 @@
 # - Package Imports -
 # ----------------------------------------------------------------------------------------------------------------------
 from AthenaGuiLib.entities.settings import Settings
-from AthenaGuiLib.entities.style_library import StyleLibrary
+
+from AthenaGuiLib.entities.style.style_library import StyleLibrary
+from AthenaGuiLib.entities.style.style_sheet import StyleSheet
 
 from AthenaGuiLib.entities.viewport import Viewport
 
