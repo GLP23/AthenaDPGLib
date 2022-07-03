7ce0eccb5fea22c62d4cc3a2236d7e2a7d487414
DirectiveAthena <directive.athena@gmail.com>
2022-07-03 14:57:46 +0200

    Cleanup

---
+++ src/AthenaGuiLib/functions/fixes.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import sys
+import ctypes
+
+# Custom Library
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def fix_icon_for_taskbar(app_model_id:str):
+    # Define application ICON,
+    #   makes sure the APPLICATION icon is shown in the taskbar
+    if sys.platform == "win32":
+        # WINDOWS NEEDS THIS to make this possible
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            self.model_id
+        )
+    else:
+        # TODO fix this! (aka, find out how to do this)
+        raise NotImplementedError
