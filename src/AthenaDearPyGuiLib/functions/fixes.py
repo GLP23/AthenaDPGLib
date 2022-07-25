4313b20a01c48f4a662e09ac6c49acf3405686b0
DirectiveAthena <directive.athena@gmail.com>
2022-07-25 14:38:30 +0200

    Change : Better error text

--- src/AthenaDearPyGuiLib/functions/fixes.py
+++ src/AthenaDearPyGuiLib/functions/fixes.py
@@ -16,9 +16,9 @@ import ctypes
 def fix_icon_for_taskbar(app_model_id:str):
     # Define application ICON,
     #   makes sure the APPLICATION icon is shown in the taskbar
-    if sys.platform == "win32":
+    if (sys_platform := sys.platform) == "win32":
         # WINDOWS NEEDS THIS to make this possible
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_model_id)
     else:
         # TODO fix this! (aka, find out how to do this)
-        raise NotImplementedError
+        raise NotImplementedError(f"the 'fix_icon_for_taskbar' function doe not work for the os: {sys_platform}")
