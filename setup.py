8d6fae25f966f962e1403865e5f49cd1793c23dd
DirectiveAthena <directive.athena@gmail.com>
2022-08-10 17:29:42 +0200

    Update: Requirements

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
     python_requires=">=3.10",
     install_requires=[
         "DearPyGui>=1.6.2",
-        "AthenaLib>=0.2.0",
-        "AthenaColor>=5.1.0"
+        "AthenaLib>=1.6.2",
+        "AthenaColor>=6.2.0"
     ]
 )
