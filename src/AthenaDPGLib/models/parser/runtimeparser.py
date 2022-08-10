e04ab48862091aa935a051ecfb120c113429430c
DirectiveAthena <directive.athena@gmail.com>
2022-08-10 16:59:34 +0200

    Change: "_children"

--- src/AthenaDPGLib/models/parser/runtimeparser.py
+++ src/AthenaDPGLib/models/parser/runtimeparser.py
@@ -4,12 +4,10 @@
 # General Packages
 from __future__ import annotations
 import dearpygui.dearpygui as dpg
-import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 import json
 
 # Custom Library
-from AthenaLib.data.text import TRUTHY
 
 # Custom Packages
 from AthenaDPGLib.data.runtimeparser_mapping import (
@@ -48,35 +46,32 @@ class RuntimeParser:
             self.document = json.load(file)
 
         match self.document["dpg"]:
-            case {"mode":0,"items":items,}:
-                self._parse_recursive(parent=items)
+            case {"mode":0,"_children":children,}:
+                self._parse_recursive(parent=children)
             case _:
                 raise RuntimeError
 
 
     def _parse_recursive(self, parent:list):
-        for item in parent: #type:dict
-            for tag, attrib in item.items(): #type: str, dict
-                if tag in RUNTIMEPARSER_MAPPING_CONTEXTMANGERS:
-                    child_items = attrib["items"]
-                    attrib.pop("items")
-                    with RUNTIMEPARSER_MAPPING_CONTEXTMANGERS[tag](**attrib):
-                        self._parse_recursive(parent=child_items)
+        for tag, attrib in ((k,v) for item in parent for k, v in item.items()): #type: str, dict
+            if tag in RUNTIMEPARSER_MAPPING_CONTEXTMANGERS:
+                children = attrib["_children"]
+                attrib.pop("_children")
+                with RUNTIMEPARSER_MAPPING_CONTEXTMANGERS[tag](**attrib):
+                    self._parse_recursive(parent=children)
 
-                elif tag in RUNTIMEPARSER_MAPPING_ITEMS_STRIPPED:
-                    if "callback" in attrib:
-                        attrib["callback"] = self.callbacks[attrib["callback"]]
-                    RUNTIMEPARSER_MAPPING_ITEMS_STRIPPED[tag](**attrib)
+            elif tag in RUNTIMEPARSER_MAPPING_ITEMS_STRIPPED:
+                if "callback" in attrib:
+                    attrib["callback"] = self.callbacks[attrib["callback"]]
+                RUNTIMEPARSER_MAPPING_ITEMS_STRIPPED[tag](**attrib)
 
-                else:
-                    # for special cases
-                    match tag:
-                        case "primary_window":
-                            child_items = attrib["items"]
-                            attrib.pop("items")
-                            with dpg.window(**attrib, tag="primary_window"):
-                                self._parse_recursive(parent=child_items)
-                            dpg.set_primary_window("primary_window", True)
+            # for special cases
+            elif tag == "primary_window":
+                children = attrib["_children"]
+                attrib.pop("_children")
+                with dpg.window(**attrib, tag="primary_window"):
+                    self._parse_recursive(parent=children)
+                dpg.set_primary_window("primary_window", True)
 
-                        case "viewport":
-                            dpg.create_viewport(**attrib)
+            elif tag == "viewport":
+                dpg.create_viewport(**attrib)
