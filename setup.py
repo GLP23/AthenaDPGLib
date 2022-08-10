330bb801b27d69b69593bcc53aa5f762bc3bbde1
DirectiveAthena <directive.athena@gmail.com>
2022-08-10 17:27:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import setuptools
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 def version_handler() -> str:
-    version = 1,0,0
+    version = 0,1,0
     return ".".join(str(i) for i in version)
 
 setuptools.setup(
