b5334d328045774722d21ddfa6015d54fd1d4063
DirectiveAthena <directive.athena@gmail.com>
2022-08-18 15:43:35 +0200

    Fix: Type Hinting custom_dpg_item

--- src/AthenaDPGLib/functions/decorators.py
+++ src/AthenaDPGLib/functions/decorators.py
@@ -3,6 +3,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # General Packages
 from __future__ import annotations
+from typing import Callable
 
 # Custom Library
 
@@ -12,11 +13,11 @@ from AthenaDPGLib.data.globals import global_custom_dpg_items
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
-def custom_dpg_item(name):
+def custom_dpg_item(name:str) -> Callable:
     """
     Decorator which assigns the method as a custom dpg item which can be used within the json ui files.
     Meant to be used within an inherited class of RunTimeParser
     """
-    def decorator(fnc):
+    def decorator(fnc:Callable):
         global_custom_dpg_items[name] = fnc
     return decorator
