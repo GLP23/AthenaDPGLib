253a13f89fe231ff66b63bbd77926f9cab4124d8
DirectiveAthena <directive.athena@gmail.com>
2022-08-22 01:05:50 +0200

    Update json_ui_parser.py

--- src/AthenaDPGLib/functions/json_ui_parser.py
+++ src/AthenaDPGLib/functions/json_ui_parser.py
@@ -12,7 +12,7 @@ from AthenaLib.data.types import PATHLIKE
 from AthenaDPGLib.data.json_ui_parser_mappings import JSONUIPARSER_ITEMS, JSONUIPARSER_CONTEXTMANGERS
 
 # ----------------------------------------------------------------------------------------------------------------------
-# - Code -
+# - Support Code -
 # ----------------------------------------------------------------------------------------------------------------------
 error_tag = lambda tag, item : ValueError(
     f"'{tag}' was already present in the tags dictionary.\nRaised in '{item}' item"
@@ -24,8 +24,45 @@ error_file = lambda filepath : ValueError(
     f"The file `{filepath}` had no usable structure"
 )
 
+def _item_and_attrib_generator(children:list) ->  tuple[str, dict] :
+    for item, attrib in ((k, v) for i in children for k, v in i.items()):
+        yield item, attrib
+
+def _recursive_parser(item: str, attrib: dict, *, custom_dpg_items: dict, tags: set):
+    """
+    Recursive part of the parser.
+    It will recursively parse all child items of DPG items that are run with a context manager (with statement).
+    """
+    if "tag" in attrib:
+        if (tag := attrib["tag"]) in tags:
+            raise error_tag(tag, item)
+        tags.add(tag)
+
+    if item in JSONUIPARSER_CONTEXTMANGERS:
+        # run the item with a context.
+        #   Else the child items will not be correctly placed within the parent item
+        with JSONUIPARSER_CONTEXTMANGERS[item](**attrib):
+            # Go over all items and it's descendants if needed
+            for item, attrib in _item_and_attrib_generator(attrib["_children"]):
+                _recursive_parser(item=item, attrib=attrib, custom_dpg_items=custom_dpg_items, tags=tags)
+
+    elif item in JSONUIPARSER_ITEMS:
+        # run the item creation normally
+        #   aka: dpg.add_...
+        JSONUIPARSER_ITEMS[item](**attrib)
+
+    # for special cases
+    #   see if the item can be found in the `custom_dpg_items` keys
+    elif item in custom_dpg_items:
+        # Custom implemented items that either don't have a "normal" dpg function
+        #   or are a collection of predefined items and procedures
+        custom_dpg_items[item](item, attrib)
+
+    else:
+        raise error_item(item)
+
 # ----------------------------------------------------------------------------------------------------------------------
-# - Support Code -
+# - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 def json_ui_parser(filepath:PATHLIKE, custom_dpg_items:dict, tags:set):
     """
@@ -35,12 +72,14 @@ def json_ui_parser(filepath:PATHLIKE, custom_dpg_items:dict, tags:set):
     Made as a standalone function to be used outside an AthenaApplication manner
     """
 
-    # Open file and  close as possible
-    with open(filepath, "r") as file:
-        document = json.load(file)
-
+    # Error catching block specifically placed here
+    #   Else all `KeyError` exceptions within the parser will be caught
+    #   Which should be done at all, as it will catch unintended stuff
     try:
+        with open(filepath, "r") as file:
+            document = json.load(file)
         # check for file structure
+        #   Will error out if the "root" key value pair structure doesn't exist
         dpg_data = document["dpg"]
     except KeyError:
         raise error_file(filepath)
@@ -52,42 +91,16 @@ def json_ui_parser(filepath:PATHLIKE, custom_dpg_items:dict, tags:set):
         #   This is meant for the future where there might eventually be multiple versions of parser,
         #       And this will ensure that the "old" ui files don't break
         case {"_parser": {"version": 0}, "_children": children, }:
-            _recursive_json_ui_parser(parent=children, custom_dpg_items=custom_dpg_items,tags=tags)
+            for item, attrib in _item_and_attrib_generator(children):
+                _recursive_parser(item=item, attrib=attrib, custom_dpg_items=custom_dpg_items, tags=tags)
 
-        # If the version hasn't been specified , it will automatically pick the newest parser
-        case {"_children": children, }:
-            _recursive_json_ui_parser(parent=children, custom_dpg_items=custom_dpg_items,tags=tags)
-
+        # No usable format could be found
         case _:
             raise error_file(filepath)
 
-def _recursive_json_ui_parser(parent:list,custom_dpg_items:dict, tags:set):
-        """
-        Recursive part of the parser.
-        It will recursively parse all child items of DPG items that are run with a context manager (with statement).
-        """
-        for item, attrib in ((k,v) for i in parent for k, v in i.items()): #type: str, dict
-            if "tag" in attrib:
-                if (tag := attrib["tag"]) in tags:
-                    raise error_tag(tag, item)
-                tags.add(tag)
-
-            if item in JSONUIPARSER_CONTEXTMANGERS:
-                # run the item with a context.
-                #   Else the child items will not be correctly placed within the parent item
-                with JSONUIPARSER_CONTEXTMANGERS[item](**attrib):
-                    _recursive_json_ui_parser(parent=attrib["_children"],custom_dpg_items=custom_dpg_items,tags=tags)
-
-            elif item in JSONUIPARSER_ITEMS:
-                # run the item creation normally
-                #   aka: dpg.add_...
-                JSONUIPARSER_ITEMS[item](**attrib)
-
-            # for special cases
-            elif item in custom_dpg_items:
-                # Custom implemented items that either don't have a "normal" dpg function
-                #   or are a collection of predefined items
-                custom_dpg_items[item](item, attrib)
-
-            else:
-                raise error_item(item)
+    # function doesn't return anything
+    #   Both `custom_dpg_items` and `tags` are expected to be mutable and don't need to be returned
+    #   No items are created, as dpg functions only create items internally
+    #       Although dpg.add_... or the context managed items do return item tags,
+    #           it is expected for any meaningfully tags to be set as attributes in the json files
+    #       This way the tags remain unique and exist "separate" from the parser
