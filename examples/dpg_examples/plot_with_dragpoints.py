dd076dd1f1cdf641a65a0a93cc19f9316346851a
Andreas Sas <directive.athena@gmail.com>
2022-08-27 18:59:05 +0200

    Update plot_with_dragpoints.py

--- examples/dpg_examples/plot_with_dragpoints.py
+++ examples/dpg_examples/plot_with_dragpoints.py
@@ -11,13 +11,16 @@ import numpy as np
 # Custom Packages
 
 # ----------------------------------------------------------------------------------------------------------------------
-# - Code -
+# - Support Code -
 # ----------------------------------------------------------------------------------------------------------------------
 # a global storage of the individual points on the plot.
 #   This list holds the dpg tags of the dragpoints, and not the individual positions
-#   If you want to have a list of all points' postions at a specific moment, you'll need to do the following:
-#       [dpg.get_value(p)[:2] for p in points]
+#   If you want to have a list of all points' postions at a specific moment
+#       you'll need to exectute the points_as_positions functions
 points:list[int] = []
+
+def points_as_pos(list_of_points:list[int]):
+    return [dpg.get_value(p)[:2] for p in list_of_points]
 
 def plot_mouseclick_left_callback():
     global points
@@ -30,6 +33,7 @@ def plot_mouseclick_left_callback():
         default_value=pos,
         callback=lambda : print("Point dragged"), # this callback is executed on dragging dragpoint
     )
+
     # After the point has been created
     #   Store the point tag somewhere (in this case auto generated)
     #   This way the point can be easily retrieved later
@@ -40,13 +44,16 @@ def plot_mouseclick_right_callback():
 
     # Use numpy to find the nearest point through a simple algorithm
     pos = dpg.get_plot_mouse_pos()
-    index_nearest = np.argmin(np.sum((np.asarray([dpg.get_value(p)[:2] for p in points]) - pos)**2, axis=1))
+    index_nearest = np.argmin(np.sum((np.asarray(points_as_pos(points)) - pos) ** 2, axis=1))
 
     # Delete the item from the plot and from the stored values
     #   Make sure to delete the removed point from the list of points as well
     dpg.delete_item(points[index_nearest])
     points.pop(index_nearest)
 
+# ----------------------------------------------------------------------------------------------------------------------
+# - Main Code -
+# ----------------------------------------------------------------------------------------------------------------------
 def main():
     """
     A simple example of how to set up a drag point handler for a plot
