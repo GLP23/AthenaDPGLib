bf26f6804d9d78cdd4387cf8d8147689e5893fba
DirectiveAthena <directive.athena@gmail.com>
2022-08-29 10:03:51 +0200

    Feat: further plot tests

--- src/AthenaDPGLib/models/landplot_designer/components/plot.py
+++ src/AthenaDPGLib/models/landplot_designer/components/plot.py
@@ -30,6 +30,13 @@ class LandplotDesigner_Plot(CustomDPGItem, LandplotDesigner_Component):
             dpg.add_plot_axis(axis=dpg.mvXAxis,tag=f"{self.tag}_x")
             dpg.add_plot_axis(axis=dpg.mvYAxis,tag=f"{self.tag}_y")
 
+            polygon = Polygon(
+                name="test",
+                colors=[127, 0, 127]
+            )
+            self.memory.selected_polygon = polygon.name
+            self.memory.polygons[polygon.name] = polygon
+
             yield plot
 
         # after everything is constructed, assign the registries
@@ -48,48 +55,35 @@ class LandplotDesigner_Plot(CustomDPGItem, LandplotDesigner_Component):
         dpg.bind_item_handler_registry(item=self.tag,handler_registry=f"{self.tag}_registry")
 
     def mousebutton_left(self):
-        # check if there is atleast a selected polygon
-        if not self.memory.selected_polygon or not self.memory.polygons:
-            polygon = Polygon(
-                name="test",
-                colors=[127, 0, 127]
-            )
-            self.memory.selected_polygon = polygon.name
-            self.memory.polygons[polygon.name] = polygon
-        else:
-            polygon = self.memory.polygons[self.memory.selected_polygon]
+        # check if there is at least a selected polygon
+        if dpg.is_mouse_button_dragging(button=dpg.mvMouseButton_Left, threshold=0.05):
+            return
 
-        self.add_drag_point()
-        self.polygon_series_update(None,None, polygon.name)
-        # x, y = dpg.get_plot_mouse_pos()
-        # self.add_polygon_series(polygon=polygon, x=[x], y=[y])
-
-
-    def add_drag_point(self):
-        # gather the actual polygon
-        polygon = self.memory.polygons[self.memory.selected_polygon]
-
-        # assign the drag point to the list of points to the polygon
-        dragpoint = dpg.add_drag_point(
-            parent=self.tag,
-            default_value=dpg.get_plot_mouse_pos(),
-            label=polygon.name,
-            color=polygon.color_node,
-            user_data=polygon.name,
-            callback=self.polygon_series_update
+        self.add_drag_point(
+            polygon=(polygon := self.memory.polygons[self.memory.selected_polygon])
         )
-        polygon.points.append(dragpoint)
+
+    def add_drag_point(self, polygon:Polygon):
+        # assign the drag point to the list of points to the polygon
+        polygon.points.append(
+            dpg.add_drag_point(
+                parent=self.tag,
+                default_value=dpg.get_plot_mouse_pos(),
+                label=polygon.name,
+                color=polygon.color_node,
+                user_data=polygon.name,
+                callback=self.polygon_series_update
+            )
+        )
+        self.polygon_series_update(dpg.last_item(),dpg.last_item(),polygon.name)
 
     # ------------------------------------------------------------------------------------------------------------------
     # - Custom Series -
     # ------------------------------------------------------------------------------------------------------------------
-    def add_polygon_series(self, *,  polygon:Polygon, x=None, y=None):
-        if dpg.does_item_exist(polygon.series):
-            dpg.delete_item(polygon.series)
-
+    def add_polygon_series(self, *,  polygon:Polygon, x:list[float|int]=None, y:list[float|int]=None):
         polygon.series = dpg.add_custom_series(
-            x=x,
-            y=y,
+            x=x if x is not None else [],
+            y=y if y is not None else [],
             channel_count=2,
             parent=f"{self.tag}_x",
             callback=self.polygon_series_callback,
@@ -97,10 +91,12 @@ class LandplotDesigner_Plot(CustomDPGItem, LandplotDesigner_Component):
             tag=f"{polygon.name}_series"
         )
 
-    def polygon_series_update(self,_, __, user_data):
+    def polygon_series_update(self,sender, app_data, user_data):
         polygon:Polygon = self.memory.polygons[user_data]
         all_positions = [(x,y) for x,y,*_ in dpg.get_values(polygon.points)]
 
+        if dpg.does_item_exist(f"{polygon.name}_series"):
+            dpg.delete_item(f"{polygon.name}_series")
 
         self.add_polygon_series(
             polygon=polygon,
@@ -112,7 +108,7 @@ class LandplotDesigner_Plot(CustomDPGItem, LandplotDesigner_Component):
         # fixes an issue that relates to quickly redrawing the series
         if not dpg.does_item_exist(sender):
             return
-
+        
         polygon:Polygon = self.memory.polygons[user_data]
 
         # gather all vars we need for the callback
@@ -135,7 +131,7 @@ class LandplotDesigner_Plot(CustomDPGItem, LandplotDesigner_Component):
         # draw the points afterwards
         #   If this is done first, these will come behind the polygon, which is not what we want
         for point in zip(transformed_x, transformed_y):
-            dpg.draw_circle(point, radius=5, fill=polygon.color_node)
+            dpg.draw_circle(point, radius=50, fill=polygon.color_node)
 
         # Always make sure to pop the container stack
         dpg.pop_container_stack()
