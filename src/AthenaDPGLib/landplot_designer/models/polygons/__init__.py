9ce084d32acb202864be45caa31a40e3ca8857e6
DirectiveAthena <directive.athena@gmail.com>
2022-09-01 14:37:32 +0200

    Removal of old code

--- src/AthenaDPGLib/landplot_designer/models/polygons/__init__.py
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------------------------------------------------------
-# - Package Imports -
-# ----------------------------------------------------------------------------------------------------------------------
-from AthenaDPGLib.landplot_designer.models.polygons.chunk_manager import ChunkManager
-from AthenaDPGLib.landplot_designer.models.polygons.chunk_of_polygons import ChunkOfPolygons
-from AthenaDPGLib.landplot_designer.models.polygons.polygon import Polygon
-from AthenaDPGLib.landplot_designer.models.polygons.coordinate import Coordinate
