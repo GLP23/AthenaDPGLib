89866601beeabd29fde98c423f90b9564f3890be
DirectiveAthena <directive.athena@gmail.com>
2022-09-27 13:53:34 +0200

    Feat: Project setup of Project Tracking Tool

---
+++ examples/project_tracking_tool/project_tracking_tool.py
@@ -0,0 +1,29 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import dearpygui.dearpygui as dpg
+
+# Custom Library
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def main():
+    dpg.create_context()
+
+    with dpg.window(tag="Primary Window"):
+        dpg.add_text("Hello, world")
+
+    dpg.create_viewport(title='Custom Title', width=600, height=200)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_primary_window("Primary Window", True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
+if __name__ == '__main__':
+    main()
